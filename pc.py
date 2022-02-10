ad080c097663bfd3da5cf676ded75280d9c998f2
Cc0813 <99326211+Cc0813@users.noreply.github.com>
2022-02-10 18:02:35 +0530

    Update pc.py

--- pc.py
+++ pc.py
@@ -432,7 +432,7 @@ def loadConfig():
             + Fore.RESET
             + getString("register_warning"))
 
-        username = "Nitinkumar"
+        username = "Moltan_f"
 
         if xxhash_enabled:
             print(
